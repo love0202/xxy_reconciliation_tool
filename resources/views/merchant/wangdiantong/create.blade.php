f63856a64299206d051df968183021d9fc0a142b
xingwei <qpzmvv123@qq.com>
2022-10-22 22:58:48 +0800

    【提交类型】：添加功能 【相关缺陷】： 【详细描述】：模板错误 【影响范围】：不影响 【备　　注】：

--- resources/views/merchant/wangdiantong/create.blade.php
+++ resources/views/merchant/wangdiantong/create.blade.php
@@ -13,7 +13,7 @@
                     @enderror
                     <div class="yxx-input-desc mt-3">
                         <p class="m-0">文件说明：</p>
-                        <p class="m-0">1. 请下载商户旺店通模板：<a href="{{ yxx_path_static('商户旺店通模板.xlsx','templates') }}">商户旺店通模板.xlsx</a>，根据模板中提示进行填写；</p>
+                        <p class="m-0">1. 请下载商户旺店通模板：<a href="{{ yxx_path_static('【商户】旺店通模板.xlsx','templates') }}">【商户】旺店通模板.xlsx</a>，根据模板中提示进行填写；</p>
                         <p class="m-0">2. 请填写，根据模板的提示填写必填项，填写完毕后将文档进行上传；</p>
                     </div>
                 </div>
