b4b0caad2b38ab70f780fe750ac8d587fbfbe865
work_xiangwei <qpzmvv123@qq.com>
2023-05-04 14:47:11 +0800

    主页面的header和footer

---
+++ src-js/next-app/src/components/layout/navbar.tsx
@@ -0,0 +1,31 @@
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+
+function BrandExample() {
+    return (
+        <header>
+            <Navbar bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand href="#home">
+                        <img
+                            alt=""
+                            src="/logo.svg"
+                            width="30"
+                            height="30"
+                            className="d-inline-block align-top"
+                        />
+                        next-app
+                    </Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#features">Features</Nav.Link>
+                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+        </header>
+    );
+}
+
+export default BrandExample;
