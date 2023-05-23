63e0d6c18c1c8b9a4715f4e5a5c55b8cc5d52f17
work_xiangwei <qpzmvv123@qq.com>
2023-05-23 16:21:26 +0800

    初始化umijs框架开发

---
+++ src-js/next-app/src/pages/about.tsx
@@ -0,0 +1,16 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <div
+      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+    >
+      <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+        about
+      </p>
+    </div>
+  )
+}
